45c7dee2f37a7ee93a48316cde672753ef25cda1
Matt Daley <m6daley@gmail.com>
2017-03-08 16:27:45 +0000

    Release stuff -> project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject astoria "0.1.0-SNAPSHOT"
   :description "Google Cloud data store client"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mdaley/astoria"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -10,6 +10,10 @@
                  [io.grpc/grpc-core "1.1.2"]
                  [io.netty/netty-codec-http2 "4.1.8.Final"]
                  [joda-time/joda-time "2.9.7"]]
+  :scm {:name "git"
+        :url "https://github.com/mdaley/astoria"}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
   :test-selectors {:default (complement (or :acceptance :integration))
                    :acceptance :acceptance
                    :integration :integration})
